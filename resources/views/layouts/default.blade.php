34b4a0e9bb485f4123ae3e1b622c3e9c1c7f8f49
Mudassar Abbas <abbas8156@gmail.com>
2021-01-18 10:23:59 +0500

    Info Links

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -129,6 +129,11 @@
             $('.slicknav_brand a').attr('href','{{ url("/") }}')
         });
     </script>
+    <script type="text/javascript">
+        var infolinks_pid = 3306601;
+        var infolinks_wsid = 0;
+    </script>
+    <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
    <!--  <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/733533b9accab7873869a1f3f/291d002c81890284b417f4504.js");</script> -->
 </body>
 </html>
